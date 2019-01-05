a9162986c852ff073f4d7ea77264e22e209bc651
Milad Roshan <roshan.milad@ymail.com>
2019-01-05 20:10:00 +0330

    end slider

--- downloadtheme/slider.php
+++ downloadtheme/slider.php
@@ -1,7 +1,32 @@
 <section id="metro-box">
+    
+    <?php
+    $c=0;
+    $q2=new WP_Query(
+        array("category_name"=>"slider_big","posts_per_page"=>3)
+    );
+    while($q2->have_posts())
+    {
+        $c++;
+        $q2->the_post();
+    ?>
+    <div class="column">
+    <div class="metro-item big">
+        <div class="box-img">
+            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+            <div class="metro-content">
+                <h3>
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> 
+                </h3>
+                <p>
+                    <?php the_excerpt(); ?>   
+                </p>
+            </div>
+        </div>
+    </div>
     <?php
     $q=new WP_Query(
-        array("category_name"=>"slider_small","posts_per_page"=>12)
+        array("category_name"=>"slider_small","posts_per_page"=>4,"paged"=>$c)
     );
     while($q->have_posts())
     {
@@ -9,151 +34,22 @@
         the_post_thumbnail();
         echo "<hr>";
     }
-    $q2=new WP_Query(
-        array("category_name"=>"slider_big","posts_per_page"=>3)
-    );
-    while($q2->have_posts())
-    {
-        $q2->the_post();
-        the_post_thumbnail();
-        echo "<hr>";
-    }
-
-
-
     ?>
-        <div class="column">
-            <div class="metro-item big">
-                <div class="box-img">
-                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/slider/1.jpg" alt=""></a>
-                    <div class="metro-content">
-                        <h3>
-                            <a href="">تماس های رایگان با LINE: Free Calls & Messages v4.1.0</a> 
-                        </h3>
-                        <p>
-                                LINE یکی از بهترین و پرطرفدارترین مسنجرهای عرضه شده برای اندروید است که به شما این قابلیت را می دهد به صورت کاملا رایگان با دوستان خود تماس صوتی برقرار کنید و یا به ارسال پیامک به صورت رایگان و نامحدود در هر ساعت از شبانه روز بپردازید....
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="metro-item">
-                <div class="box-img">
-                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/slider/2.jpg" alt=""></a>
-                    <div class="metro-content color-blue">
-                        <h3>
-                            <a href="">تماس های رایگان با LINE: Free Calls & Messages v4.1.0</a> 
-                        </h3>
-                        <p>
-                                LINE یکی از بهترین و پرطرفدارترین مسنجرهای عرضه شده برای اندروید است که به شما این قابلیت را می دهد به صورت کاملا رایگان با دوستان خود تماس صوتی برقرار کنید و یا به ارسال پیامک به صورت رایگان و نامحدود در هر ساعت از شبانه روز بپردازید....
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="metro-item">
-                <div class="box-img">
-                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/slider/3.jpg" alt=""></a>
-                    <div class="metro-content color-red">
-                        <h3>
-                            <a href="">تماس های رایگان با LINE: Free Calls & Messages v4.1.0</a> 
-                        </h3>
-                        <p>
-                                LINE یکی از بهترین و پرطرفدارترین مسنجرهای عرضه شده برای اندروید است که به شما این قابلیت را می دهد به صورت کاملا رایگان با دوستان خود تماس صوتی برقرار کنید و یا به ارسال پیامک به صورت رایگان و نامحدود در هر ساعت از شبانه روز بپردازید....
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="metro-item">
-                <div class="box-img">
-                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/slider/4.jpg" alt=""></a>
-                    <div class="metro-content color-carrot">
-                        <h3>
-                            <a href="">تماس های رایگان با LINE: Free Calls & Messages v4.1.0</a> 
-                        </h3>
-                        <p>
-                                LINE یکی از بهترین و پرطرفدارترین مسنجرهای عرضه شده برای اندروید است که به شما این قابلیت را می دهد به صورت کاملا رایگان با دوستان خود تماس صوتی برقرار کنید و یا به ارسال پیامک به صورت رایگان و نامحدود در هر ساعت از شبانه روز بپردازید....
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="metro-item">
-                <div class="box-img">
-                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/slider/5.jpg" alt=""></a>
-                    <div class="metro-content">
-                        <h3>
-                            <a href="">تماس های رایگان با LINE: Free Calls & Messages v4.1.0</a> 
-                        </h3>
-                        <p>
-                                LINE یکی از بهترین و پرطرفدارترین مسنجرهای عرضه شده برای اندروید است که به شما این قابلیت را می دهد به صورت کاملا رایگان با دوستان خود تماس صوتی برقرار کنید و یا به ارسال پیامک به صورت رایگان و نامحدود در هر ساعت از شبانه روز بپردازید....
-                        </p>
-                    </div>
-                </div>
+    <div class="metro-item">
+        <div class="box-img">
+            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+            <div class="metro-content color-blue">
+                <h3>
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a> 
+                </h3>
+                <p>
+                <?php the_excerpt(); ?>     
+                </p>
             </div>
         </div>
-        <div class="column">
-            <div class="metro-item big">
-                <div class="box-img">
-                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/slider/1.jpg" alt=""></a>
-                    <div class="metro-content">
-                        <h3>
-                            <a href="">تماس های رایگان با LINE: Free Calls & Messages v4.1.0</a> 
-                        </h3>
-                        <p>
-                                LINE یکی از بهترین و پرطرفدارترین مسنجرهای عرضه شده برای اندروید است که به شما این قابلیت را می دهد به صورت کاملا رایگان با دوستان خود تماس صوتی برقرار کنید و یا به ارسال پیامک به صورت رایگان و نامحدود در هر ساعت از شبانه روز بپردازید....
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="metro-item">
-                <div class="box-img">
-                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/slider/5.jpg" alt=""></a>
-                    <div class="metro-content">
-                        <h3>
-                            <a href="">تماس های رایگان با LINE: Free Calls & Messages v4.1.0</a> 
-                        </h3>
-                        <p>
-                                LINE یکی از بهترین و پرطرفدارترین مسنجرهای عرضه شده برای اندروید است که به شما این قابلیت را می دهد به صورت کاملا رایگان با دوستان خود تماس صوتی برقرار کنید و یا به ارسال پیامک به صورت رایگان و نامحدود در هر ساعت از شبانه روز بپردازید....
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="metro-item">
-                <div class="box-img">
-                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/slider/4.jpg" alt=""></a>
-                    <div class="metro-content color-carrot">
-                        <h3>
-                            <a href="">تماس های رایگان با LINE: Free Calls & Messages v4.1.0</a> 
-                        </h3>
-                        <p>
-                                LINE یکی از بهترین و پرطرفدارترین مسنجرهای عرضه شده برای اندروید است که به شما این قابلیت را می دهد به صورت کاملا رایگان با دوستان خود تماس صوتی برقرار کنید و یا به ارسال پیامک به صورت رایگان و نامحدود در هر ساعت از شبانه روز بپردازید....
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="metro-item">
-                <div class="box-img">
-                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/slider/3.jpg" alt=""></a>
-                    <div class="metro-content color-red">
-                        <h3>
-                            <a href="">تماس های رایگان با LINE: Free Calls & Messages v4.1.0</a> 
-                        </h3>
-                        <p>
-                                LINE یکی از بهترین و پرطرفدارترین مسنجرهای عرضه شده برای اندروید است که به شما این قابلیت را می دهد به صورت کاملا رایگان با دوستان خود تماس صوتی برقرار کنید و یا به ارسال پیامک به صورت رایگان و نامحدود در هر ساعت از شبانه روز بپردازید....
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div class="metro-item">
-                <div class="box-img">
-                    <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/slider/2.jpg" alt=""></a>
-                    <div class="metro-content color-blue">
-                        <h3>
-                            <a href="">تماس های رایگان با LINE: Free Calls & Messages v4.1.0</a> 
-                        </h3>
-                        <p>
-                                LINE یکی از بهترین و پرطرفدارترین مسنجرهای عرضه شده برای اندروید است که به شما این قابلیت را می دهد به صورت کاملا رایگان با دوستان خود تماس صوتی برقرار کنید و یا به ارسال پیامک به صورت رایگان و نامحدود در هر ساعت از شبانه روز بپردازید....
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
+    </div>
+
+    <?php
+        }
+    ?>
     </section>
